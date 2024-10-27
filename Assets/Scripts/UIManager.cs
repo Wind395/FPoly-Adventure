2e7d81af50399414e58966f6dee4c425e5402f7c
Wind395 <phongntph49930@gmail.com>
2024-10-27 14:00:40 +0700

    Added Opening

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -21,8 +21,11 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        if (SceneManager.GetActiveScene().name == "Ending") {
-            StartCoroutine(ShowUI());
+        if (SceneManager.GetActiveScene().name == "P203" && ConversationManager.nextConversation == 0) {
+            StartCoroutine(OpeningShowUI());
+        }
+        if (SceneManager.GetActiveScene().name == "Ending" && ConversationManager.nextConversation >= 3) {
+            StartCoroutine(EndingShowUI());
         }
     }
 
@@ -30,17 +33,45 @@ public class UIManager : MonoBehaviour
         StartCoroutine(Ending2UI());
     }
 
-    private IEnumerator ShowUI()
+    private IEnumerator OpeningShowUI()
     {
         // Mặc định màu đen
         uiGroup.alpha = 1f;
 
         // Hiện text đầu tiên
-        displayText.text = "Khi tỉnh lại, cậu thấy bản thân đã trở về thế giới thực và được thầy giáo gọi dậy.";
+        displayText.text = textContent[0];
+        yield return new WaitForSeconds(displayDuration);
+        displayText.text = textContent[1];
+        yield return new WaitForSeconds(displayDuration);
+        displayText.text = textContent[2];
+        yield return new WaitForSeconds(displayDuration);
+        displayText.text = textContent[3];
+        yield return new WaitForSeconds(displayDuration);
+        displayText.text = textContent[4];
+        // Chuyển đổi mờ dần
+        float elapsedTime = 0f;
+        while (elapsedTime < transitionDuration)
+        {
+            uiGroup.alpha = 1 - (elapsedTime / transitionDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        uiGroup.alpha = 0f; // Đặt alpha về 0 sau khi chuyển đổi
+        UI1.SetActive(false);
+    }
+
+    private IEnumerator EndingShowUI()
+    {
+        // Mặc định màu đen
+        uiGroup.alpha = 1f;
+
+        // Hiện text đầu tiên
+        displayText.text = textContent[0];
         yield return new WaitForSeconds(displayDuration);
 
         // Hiện text thứ hai
-        displayText.text = "Sau tiết học....";
+        displayText.text = textContent[1];
         // Chuyển đổi mờ dần
         float elapsedTime = 0f;
         while (elapsedTime < transitionDuration)
